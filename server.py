cc05a27bde32c8814ad2e2c4d183db25aa7ca93f
Liam <liamsycamore@gmail.com>
2016-04-27 13:45:26 +1200

    password required

--- server.py
+++ server.py
@@ -11,15 +11,23 @@ s.bind((host, port))
 s.listen(1)
 conn, addr = s.accept()
 user = conn.recv(1024)
-print ("Connection from: " + (str(addr[0])) + "\nUsername: " + (user))
+psswd = conn.recv(1024)
+if psswd == 'pi':
+	conn.sendall("Connection accepted")
+	print ("Connection from: " + (str(addr[0])) + "\nUsername: " + (user))
 
-# Messaging loop
-while True:
-    data = conn.recv(1024)
-    if not data: break
-    print((user)+": "+(data))
-    response = raw_input("Server: ")
-    if response == "exit":
-        break
-    conn.sendall(response)
-conn.close()
+	# Messaging loop
+	while True:
+    		data = conn.recv(1024)
+   		if not data: break
+    		print((user)+": "+(data))
+    		response = raw_input("Server: ")
+    		if response == "exit":
+        		break
+    		conn.sendall(response)
+	conn.close()
+else:
+	print ("Connection refused")
+	print ("Shutting down")
+        conn.sendall("Connection refused")
+	conn.close()
